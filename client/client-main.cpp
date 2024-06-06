0f2d2761a84743dee99b91423ceb23278f7b6cb0
Andrew Drogalis <108765079+andrew-drogalis@users.noreply.github.com>
2024-06-06 13:20:49 -0400

    Added EXPORT_COMPILE_COMMANDS

--- client/client-main.cpp
+++ client/client-main.cpp
@@ -4,27 +4,27 @@
 #include <iostream>// for operator<<, basic_ostream, cout
 #include <string>  // for basic_string
 
-#include "main-utilities-client.hpp"// for validateMainParameters
-#include "tcp-client.h"             // for TCPClient
+#include "tcp-client/include/tcp-client.h" for validateMainParameters
+#include "tcp-client/include/main-utilities-client.hpp" for TCPClient
 
 int main(int argc, char* argv[])
 {
     std::string IP_ADDRESS = "127.0.0.1";
     int PORT = 54000, BUFFER_SIZE = 1024;
 
-    if (! tcpclient::validateMainParameters(argc, argv, IP_ADDRESS, PORT, BUFFER_SIZE))
+    if (! fixclient::validateMainParameters(argc, argv, IP_ADDRESS, PORT, BUFFER_SIZE))
     {
         return 1;
     }
     // -------------------
-    tcpclient::TCPClient tcpClient {IP_ADDRESS, PORT, BUFFER_SIZE};
+    fixclient::FIXClient fixClient {IP_ADDRESS, PORT, BUFFER_SIZE};
 
-    auto tcp_client_response = tcpClient.start_client();
+    auto fix_client_response = fixClient.start_client();
 
-    if (! tcp_client_response)
+    if (! fix_client_response)
     {
-        std::cout << "Error Location: " << tcp_client_response.error().where() << '\n';
-        std::cout << tcp_client_response.error().what() << '\n';
+        std::cout << "Error Location: " << fix_client_response.error().where() << '\n';
+        std::cout << fix_client_response.error().what() << '\n';
         return 1;
     }
 
--- server/server-main.cpp
+++ server/server-main.cpp
@@ -3,27 +3,27 @@
 
 #include <iostream>// for operator<<, basic_ostream, cout
 
-#include "main-utilities-server.hpp"// for validateMainParameters
-#include "tcp-server.h"             // for TCPServer
+#include "tcp-server/include/main-utilities-server.hpp" for validateMainParameters
+#include "fix-server.h"             // for TCPServer
 
 int main(int argc, char* argv[])
 {
     // User Default Values
     int PORT = 54000, MAX_CLIENTS = 10, MAX_EVENTS = 10, BUFFER_SIZE = 1024;
 
-    if (! tcpserver::validateMainParameters(argc, argv, PORT, MAX_CLIENTS, MAX_EVENTS, BUFFER_SIZE))
+    if (! fixserver::validateMainParameters(argc, argv, PORT, MAX_CLIENTS, MAX_EVENTS, BUFFER_SIZE))
     {
         return 1;
     }
     // -------------------
-    tcpserver::TCPServer tcpServ {PORT, MAX_CLIENTS, MAX_EVENTS, BUFFER_SIZE};
+    fixserver::FIXServer fixServ {PORT, MAX_CLIENTS, MAX_EVENTS, BUFFER_SIZE};
 
-    auto tcp_server_response = tcpServ.start_server();
+    auto fix_server_response = fixServ.start_server();
 
-    if (! tcp_server_response)
+    if (! fix_server_response)
     {
-        std::cout << "Error Location: " << tcp_server_response.error().where() << '\n';
-        std::cout << tcp_server_response.error().what() << '\n';
+        std::cout << "Error Location: " << fix_server_response.error().where() << '\n';
+        std::cout << fix_server_response.error().what() << '\n';
         return 1;
     }
 
